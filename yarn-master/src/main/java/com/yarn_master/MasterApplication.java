5cca22448b4ce441c34fbef331d4e3c98baf0712
geoff <mac@macs-MacBook-Pro.local>
2020-10-02 23:21:06 +0300

    Added master class

---
+++ yarn-master/src/main/java/com/yarn_master/MasterApplication.java
@@ -0,0 +1,11 @@
+package com.yarn_master;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class MasterApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MasterApplication.class, args);
+    }
+}
