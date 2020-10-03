91312aef160d6034bab7f26f59f1fd63b208f954
geoff <mac@macs-MacBook-Pro.local>
2020-10-04 01:25:57 +0300

    Updated Appmaster config

--- yarn_appmaster/src/main/java/com/yarn_appmaster/AppApplication.java
+++ yarn_appmaster/src/main/java/com/yarn_appmaster/AppApplication.java
@@ -2,15 +2,10 @@ package com.yarn_appmaster;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.yarn.client.YarnClient;
 
 @EnableAutoConfiguration
 public class AppApplication {
-
     public static void main(String[] args) {
-        SpringApplication.run(AppApplication.class, args)
-                .getBean(YarnClient.class)
-                .submitApplication();
+        SpringApplication.run(AppApplication.class, args);
     }
-
 }
