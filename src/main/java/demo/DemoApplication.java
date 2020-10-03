715318360867f134693c16f384be225bdb727a9d
geoff <mac@macs-MacBook-Pro.local>
2020-10-03 20:34:23 +0300

    Updated configs

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -1,23 +1,4 @@
-package com.yarndemo.demo;
-
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@SpringBootApplication
-//@RestController
-//public class DemoApplication {
-//
-//	@RequestMapping("/")
-//	String home() {
-//		return "Hello World!";
-//	}
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(DemoApplication.class, args);
-//	}
-//}
+package demo;
 
 import org.apache.hadoop.fs.FileStatus;
 import org.springframework.beans.factory.annotation.Autowired;
