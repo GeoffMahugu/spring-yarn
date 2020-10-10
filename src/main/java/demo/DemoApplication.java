b00b98a1aa010c519fd6840095e0998d20d67d12
geoff <mac@macs-MacBook-Pro.local>
2020-10-10 17:14:12 +0300

    Added bean for autowire FsShell

--- src/main/java/demo/DemoApplication.java
+++ src/main/java/demo/DemoApplication.java
@@ -5,10 +5,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.hadoop.fs.FsShell;
 
 @SpringBootApplication
 public class DemoApplication implements CommandLineRunner {
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 
 	@Autowired
 	private FsShell shell;
@@ -20,7 +24,11 @@ public class DemoApplication implements CommandLineRunner {
 		}
 	}
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	@Bean
+	FsShell shell(){
+		org.apache.hadoop.conf.Configuration hadoopConfiguration = new org.apache.hadoop.conf.Configuration();
+		hadoopConfiguration.set("fs.defaultFs", "hdfs://localhost:9000");
+		return new FsShell(hadoopConfiguration);
 	}
+
 }
